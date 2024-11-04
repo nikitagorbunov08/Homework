074e97fd578d068958ff92ae63600f8c2edec754
nikitagorbunov08 <gorbunovnikita638@gmail.com>
2024-11-04 19:07:06 +0300

    Add files via upload
    Telegrammbot homework

---
+++ config.py
@@ -0,0 +1,2 @@
+keys = {'Биткоин': 'BTC', 'Эфириум': 'ETC', 'Доллар': 'USD', 'Евро': 'EUR', 'Рубли': 'RUB', }
+TOKEN = '7993093618:AAGRDuB3r5fcd3ZuAdz43k7K8yJFvIPGeNc'
---
+++ crypto.py
@@ -0,0 +1,36 @@
+import telebot
+from config import keys, TOKEN
+from extensions import ConvertionException, CryptoConverter
+bot = telebot.TeleBot(TOKEN)
+@bot.message_handler(commands=['start','help'])
+def help(message: telebot):
+    text = 'Что бы начать работу введите комманду боту в следующем формате :\n<Имя валюты> \
+    <В какую валюту перевести?> \
+<Количество переводимой валюты>\nУвидеть список всех достпных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def velues(message: telebot.types.Message):
+     text = 'Доступные валюты:'
+     for key in keys.keys():
+         text = '\n'.join((text, key, ))
+     bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+        if len(values) != 3:
+            raise ConvertionException('Слишком много параметров.')
+        quoyte, base, amount = values
+        total_base = CryptoConverter.convert(quoyte, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+     bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:
+     text = f'Цена {amount} {quoyte} в {base} - {total_base}'
+     bot.send_message(message.chat.id, text)
+bot.polling()
